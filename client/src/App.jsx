250e109adcdd241d1360fbac52628a000f814322
Petro Khanenko <p.khanenko@gmail.com>
2020-12-17 23:49:16 +0200

    fucking commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import './App.css'
+import {Route, BrowserRouter} from "react-router-dom";
+
+function App() {
+    const [go, setGo] = useState(false)
+
+    const goToSite = () => {
+        setGo(true)
+    }
+
+    return (
+        <BrowserRouter>
+            <div className={"App"} onMouseOver={goToSite} onClick={goToSite}>
+                {go &&
+                    <Route path='/' component={() => { window.location = 'https://ad.admitad.com/g/r889udt5ea7ae3e44f49a499575f91/?ulp={lpurl}&pt=1'; return null;} }/>}
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App
